1ecb1436c37a1b022a462ce6d2e73d3be5e6f068
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2016-09-27 09:30:41 +0900

    added script to draw plot

---
+++ script.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+# 読み込んだCSVの最後の5行を表示
+df = pd.read_csv('https://archive.ics.uci.edu/ml/'
+        'machine-learning-databases/iris/iris.data', header=None)
+df.tail()
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# データ整形
+
+# 1-100行の目的変数の抽出
+y = df.iloc[0:100, 4].values
+# Iris-setosaを-1、Iris-versicolorを1に変換
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+# 1-100行の1,3列の抽出
+X = df.iloc[0:100, [0, 2]].values
+
+# setosaのプロット
+plt.scatter(X[:50,0], X[:50,1], color='red', marker='o', label='setosa')
+# versicolorのプロット
+plt.scatter(X[50:100,0], X[50:100,1], color='blue', marker='x', label='versicolor')
+# 軸の設定
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+# 凡例の設定
+plt.legend(loc='upper left')
+# 図の表示
+plt.show()
