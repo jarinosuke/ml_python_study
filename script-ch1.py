6b0727c5aa9dff7af7da39cfd8659a2dbf3af323
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2016-09-29 09:16:52 +0900

    implemented drawing decision region

--- script-ch1.py
+++ script-ch1.py
@@ -45,3 +45,42 @@ plt.ylabel('Number of misclassifications')
 # 図の表示
 plt.show()
 
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution=0.02) :
+    # マーカーとカラーマップの準備
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # 決定領域のプロット
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    # グリッドポイントの生成
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+
+    # 各特徴量を1次元配列に変換して予測を実行
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+
+    # 予測結果をもとのグリッドポイントのデータサイズに変換
+    Z = Z.reshape(xx1.shape)
+
+    # グリッドポイントの等高線のプロット
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+
+    # 軸の範囲指定
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # クラスごとにサンプルをプロット
+    for idx, cl in enumerate(np.unique(y)) :
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
+
+
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+plt.show()
