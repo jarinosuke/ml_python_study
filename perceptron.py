447e23ac2325ede1f7c83b3cb60156eccae1b735
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2017-04-06 09:27:43 +0900

    addedd ADALINE implementation

--- perceptron.py
+++ perceptron.py
@@ -113,3 +113,75 @@ plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Number of misclassifications')
 plt.show()
+
+
+class AdalineGD(object):
+
+    def __init__(self, eta=0.01, n_iter=50):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            # 活性化関数の計算
+            output = self.net_input(X)
+
+            # 誤差の計算
+            errors = y - output
+
+            # w の更新
+            self.w_[1:] += self.eta * X.T.dot(errors)
+
+            # w0 の更新
+            self.w_[0] += self.eta * errors.sum()
+
+            # cost 関数の計算
+            cost = (errors ** 2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+    # 総入力の計算
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    # 線形活性化関数の出力を計算
+    def activation(self, X):
+        return self.net_input(X)
+
+    def predict(self, X):
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+ax[0].plot(range(1, len(ada1.cost_)+1), np.log10(ada1.cost_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.cost_)+1), np.log10(ada2.cost_), marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('log(Sum-squared-error)')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+
+plt.show()
+
+X_std = np.copy(X)
+X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
+X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
+
+ada = AdalineGD(n_iter=15, eta=0.01).fit(X_std, y)
+# plot_decision_regions(X_std, y, classifier=ada) not implemented this function
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [standalized]')
+plt.ylabel('petal length [standalized]')
+plt.legend(loc='upper left')
+plt.show()
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
+plt.show()
